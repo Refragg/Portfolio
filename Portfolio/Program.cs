137be9bc076147aebf7a5eae86cc20d57437bbf3
Refragg <francartjeremy51@gmail.com>
2024-05-02 15:30:00 +0200

    Commit initial
    Ce commit ajoute les fichiers de la première version de l'application.

---
+++ Portfolio/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.FileProviders;
+using Portfolio.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    RequestPath = "/files",
+    FileProvider = new PhysicalFileProvider("/Files/")
+    
+    // Non docker testing
+    //FileProvider = new PhysicalFileProvider("path/to/files/folder")
+});
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
