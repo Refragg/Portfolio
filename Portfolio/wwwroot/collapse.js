4ae363c2ee86bb336edc9b92f2f306391263a2d2
Refragg <francartjeremy51@gmail.com>
2024-10-24 14:43:21 +0200

    Changement de la gestion des blocs collapse
    Le code des blocs Bootstrap collapse est maintenant simplifié et plus facilement adaptable et utilisable dans d'autres pages.

---
+++ Portfolio/wwwroot/collapse.js
@@ -0,0 +1,8 @@
+﻿function toggleCollapsable(collapseElementId, arrowElementId) {
+    // Only trigger the collapse if we're not already collapsing
+    if (document.getElementById(collapseElementId).classList.contains("collapsing"))
+        return
+
+    new bootstrap.Collapse('#' + collapseElementId).toggle()
+    document.getElementById(arrowElementId).toggleAttribute("collapsed")
+}
